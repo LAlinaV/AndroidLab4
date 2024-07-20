c1ffbfeeb7dae4aeeb4951e524d7386ef0e584cc
LAlinaV <ladik151104@gmail.com>
2024-07-20 18:25:50 +0300

    realize all methods

--- task4.1.1/app/src/main/java/com/example/task411/MainActivity.java
+++ task4.1.1/app/src/main/java/com/example/task411/MainActivity.java
@@ -8,15 +8,17 @@ import androidx.core.view.GestureDetectorCompat;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener {
     GestureDetectorCompat mDetector;
-
+    TextView tvOutput ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvOutput  = findViewById(R.id.tv);
         mDetector = new GestureDetectorCompat(this,this);
         mDetector.setOnDoubleTapListener(this);
 
@@ -29,47 +31,53 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onSingleTapConfirmed(@NonNull MotionEvent e) {
-        return false;
+        tvOutput.setText(String.format("onSingleTapConfirmed: %s", e));
+        return true;
     }
 
     @Override
     public boolean onDoubleTap(@NonNull MotionEvent e) {
-        return false;
+        tvOutput.setText(String.format("onDoubleTap: %s", e));
+        return true;
     }
 
     @Override
     public boolean onDoubleTapEvent(@NonNull MotionEvent e) {
-        return false;
+        tvOutput.setText(String.format("onDoubleTapEvent: %s", e));
+        return true;
     }
 
     @Override
     public boolean onDown(@NonNull MotionEvent e) {
+        tvOutput.setText(String.format("onDown: %s", e));
         return false;
     }
 
     @Override
     public void onShowPress(@NonNull MotionEvent e) {
-
+        tvOutput.setText(String.format("onShowPress: %s", e));
     }
 
     @Override
     public boolean onSingleTapUp(@NonNull MotionEvent e) {
-        return false;
+        tvOutput.setText(String.format("onSingleTapUp: %s", e));
+        return true;
     }
 
     @Override
     public boolean onScroll(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
-        return false;
+        tvOutput.setText(String.format("onScroll: %s%s", e1.toString(), e2));
+        return true;
     }
 
     @Override
     public void onLongPress(@NonNull MotionEvent e) {
-
+        tvOutput.setText(String.format("onLongPress: %s", e));
     }
-
 
     @Override
     public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
-        return false;
+        tvOutput.setText(String.format("onFling: %s%s", e1.toString(), e2));
+        return true;
     }
 }
